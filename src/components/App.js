18be3bd554b6ab1bc74207143870f1580469e0d5
Dean <dean.juricic@gmail.com>
2019-09-02 18:46:03 +0200

    Prepare to handle network search request in App component
    SearchBar should only hanlde user input but not do network requests.
    That is why we are sending function onSearchSubmit as a prop from parent
    component to child, so child can envoke this method when the user
    submits the form.
    This way network request is done in App component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-	return (
-		<div className="ui container" style={{ marginTop: '10px' }}>
-			<SearchBar />
-		</div>
-	);
+class App extends React.Component {
+	
+	/*This function we pass in as prop to SearchBar component.
+	* Over there user input is handled and when Enter is pressed
+	* this function is envoked in callback method.
+	* We then do network request from here based on user input term
+	* in SearchBar component
+	*/
+	onSearchSubmit(term) {
+		console.log('Working! Term is: '+ term);
+	}
+	
+	render() {
+		return (
+			<div className="ui container" style={{ marginTop: '10px' }}>
+				<SearchBar onSearchSubmit = {this.onSearchSubmit}/>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,10 +4,21 @@ class SearchBar extends React.Component {
 
 	state = { term: '' };
 
-	render() {
+	/*This function is callback method to handle Enter key being pressed
+	* while focus is in input field (equivalent of submit button).
+	* We prevent default browser behavior where page is refreshed 
+	* automaticaly and then we invoke parent component method which will do the 
+	* network request based on search done in this Component. This method was received
+	* as a Prop.
+	*/
+	onFormSubmit = (event) => {
+		event.preventDefault();
+		this.props.onSearchSubmit(this.state.term);
+	}
+	render(props) {
 		return (
 			<div className="ui segment">
-				<form className="ui form">
+				<form className="ui form" onSubmit={this.onFormSubmit}>
 					<div className="field">
 						<label>Image Search</label>
 						<input
